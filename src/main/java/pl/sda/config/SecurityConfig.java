842b78b0f3b4b40756c6ebc23ccfaf8d2b57debe
SzymSie <sieklucki.szymon@gmail.com>
2018-07-08 23:47:04 +0200

    Car tab signedout access denial fix

--- src/main/java/pl/sda/config/SecurityConfig.java
+++ src/main/java/pl/sda/config/SecurityConfig.java
@@ -46,7 +46,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/favicon.ico", "/resources/**", "/signup", "/car").permitAll()
+                .antMatchers("/", "/favicon.ico", "/resources/**", "/signup").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
